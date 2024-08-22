d49db1ba7f2f0e35681a360e17b9247823694ddf
Alberto Soto Fernandez <alberto.soto@magnolia-cms.com>
2024-08-22 14:44:19 +0200

    introduce quarkus helper CLI with dynamic values

--- k8s/quarkus-operator-launcher.sh
+++ k8s/quarkus-operator-launcher.sh
@@ -35,28 +35,40 @@ while true; do
       ;;
     2 )
       echo "-->   2 - Setup project & organization name"
+      read -p "Enter the group ID (e.g., info.magnolia.k8s): " GROUP_ID
+      read -p "Enter the project ID (e.g., quarkus-operator-demo): " PROJECT_ID
+      echo "GROUP_ID set to: $GROUP_ID"
+      echo "PROJECT_ID set to: $PROJECT_ID"
       ;;
     3 )
       echo "-->   3 - Generate quarkus operator project"
-      quarkus create app info.magnolia:qosdk-test-olm -x='qosdk,olm'
+      quarkus create app "$GROUP_ID:$PROJECT_ID" -x='qosdk,olm'
       ;;
     4 )
       echo "-->   4 - Project dev"
+      cd "$PROJECT_ID" || exit
       quarkus dev
+      cd ..
       ;;
     5 )
       echo "-->   5 - Compile the project"
-      mvn clean package -Dquarkus.container-image.build=true -Dquarkus.container-image.registry=localhost:5001 -Dquarkus.container-image.group='' -Dquarkus.container-image.name='message-broker-operator-local' -Dquarkus.container-image.tag=latest
+      cd "$PROJECT_ID" || exit
+      mvn clean package -Dquarkus.container-image.build=true -Dquarkus.container-image.registry=localhost:5001 -Dquarkus.container-image.group='' -Dquarkus.container-image.name="$PROJECT_ID" -Dquarkus.container-image.tag=latest
+      cd ..
       ;;
     6 )
       echo "-->   6 - Generate docker image and publish locally"
-      docker build -t message-broker-operator-local -f ./src/main/docker/Dockerfile.jvm .
-      docker tag message-broker-operator-local:latest localhost:5001/message-broker-operator-local:latest
-      docker push localhost:5001/message-broker-operator-local:latest
+      cd "$PROJECT_ID" || exit
+      docker build -t "$PROJECT_ID" -f ./src/main/docker/Dockerfile.jvm .
+      docker tag "$PROJECT_ID":latest localhost:5001/"$PROJECT_ID":latest
+      docker push localhost:5001/"$PROJECT_ID":latest
+      cd ..
       ;;
     7 )
       echo "-->   7 - deploy to current kubernetes cluster"
-      kubectl apply -f target/kubernetes/kubernetes.yml
+      cd "$PROJECT_ID" || exit
+      kubectl apply -f ./target/kubernetes/kubernetes.yml
+      cd ..
       ;;
     [Nn]* )
       echo "Exiting..."
